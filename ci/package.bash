d83cf988baa93e237ec0ae8520308225a767a64b
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-12-18 13:29:19 -0500

    Move library binaries into a subdirectory

--- ci/package.bash
+++ ci/package.bash
@@ -1,3 +1,3 @@
-cp $1/*/lib/*.so    project/addons/pure-data/
-cp $1/*/lib/*.dll   project/addons/pure-data/
-cp $1/*/lib/*.dylib project/addons/pure-data/
+cp $1/*/lib/*.so    project/addons/pure-data/bin/
+cp $1/*/lib/*.dll   project/addons/pure-data/bin/
+cp $1/*/lib/*.dylib project/addons/pure-data/bin/
